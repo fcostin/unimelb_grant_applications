a82354da70ae9ba76ea2677b9e1e5bf282430462
fcostin <reuben.fletchercostin@gmail.com>
2011-01-06 22:21:30 +1100

    enough for tonight

--- example_glm.r
+++ example_glm.r
@@ -1,45 +1,61 @@
 # anthony goldblum's (of kaggle) sample R code
 
-traindat <- read.csv('unimelb_training.csv',header=TRUE)
-testdat  <- read.csv('unimelb_test.csv',header=TRUE)
+load_unimelb_data = function(data) {
+	# modify data in place so it's more understandable for prediction routines
+	is.na(data)[data == ""] <- TRUE # replace blank cells with NAs
 
-blank <- traindat == "" #find all blank cells 
-is.na(traindat)[blank] <- TRUE #change them to NAs
-blank <- testdat == "" #find all blank cells 
-is.na(testdat)[blank] <- TRUE #change them to NAs
-
-personIDs<-c(27,42,57,72,87,102,117,132,147,162,177,192,207,222,237,252)
-nv0 <- c(personIDs+10,personIDs+11,personIDs+12,personIDs+13,personIDs+14,personIDs+9) #numerical values that should be 0
-nv <- c(1,8,10,12,14,16,18,20,22,24,26,nv0)  #numerical values
-
-for (i in 1:ncol(traindat)) {
-    if (i %in% nv) {
-        traindat[,i] <- as.numeric(as.character(traindat[,i]))
-        testdat[,i] <- as.numeric(as.character(testdat[,i]))
-        #if (i %in% nv0) # 
-            #A[is.na(A[,i]),i] <- 0 
-    } else if (i == 6 ) { #dates
-        #traindat[,i] <- as.Date(traindat[,i],"%d-%b-%y")
-        #testdat[,i] <- as.Date(testdat[,i],"%d-%b-%y")
-        traindat[,i] <- as.Date(traindat[,i])
-        testdat[,i] <- as.Date(testdat[,i])
-    } else {
-        traindat[,i] <- as.factor(traindat[,i])
-        testdat[,i] <- as.factor(testdat[,i])
-    }
+	personIDs<-c(27,42,57,72,87,102,117,132,147,162,177,192,207,222,237,252)
+	nv0 <- c(personIDs+10,personIDs+11,personIDs+12,personIDs+13,personIDs+14,personIDs+9) #numerical values that should be 0
+	nv <- c(1,8,10,12,14,16,18,20,22,24,26,nv0)  #numerical values
+	for (i in 1:ncol(data)) {
+		if (i %in% nv) {
+			data[,i] <- as.numeric(as.character(data[,i]))
+		} else if (i == 6 ) { #dates
+			data[,i] <- as.Date(data[,i])
+		} else {
+			data[,i] <- as.factor(data[,i])
+		}
+	}
+	return(data)
 }
-#RF.predict(RF,testdat[,-c(1,5,removeVar)])
 
-require('ROCR')
+get_unimelb_data = function(csv_file_name) {
+	cache_file_name <- paste(csv_file_name, 'cache', sep = '.')
+	if (file.exists(cache_file_name)) {
+		print(paste('loading', csv_file_name, 'from cache file'))
+		return(read.csv(cache_file_name, header = TRUE))
+	} else {
+		print(paste('loading', csv_file_name))
+		data <- read.csv(csv_file_name, header = TRUE)
+		print('converting data to more reasonable format')
+		nice_data <- load_unimelb_data(data)
+		print('caching nicer data for next time ...')
+		write.csv(nice_data, file = cache_file_name)
+		return(nice_data)
+	}
+}
 
-logit.fit <- glm(Grant.Status ~ 
-                             + Number.of.Successful.Grant 
-                             + Number.of.Unsuccessful.Grant
-							 + Grant.Application.ID
-							 #+ Grant.Category.Code	
-							 #+ Contract.Value.Band...see.note.A
-							,
-                 data = traindat,
-                 family = binomial(link = 'logit'))
+traindat <- get_unimelb_data('data/unimelb_training.csv')
+testdat  <- get_unimelb_data('data/unimelb_test.csv')
 
-GLMPred <- predict(logit.fit,testdat,type = "response")
+# use randomForest to fill in missing values (ie most of the table)
+library(randomForest)
+print('using randomForest to impute missing values...')
+traindat.imputed <- rfImpute(Grant.Status ~ ., traindat)
+print('...done imputing.')
+
+
+
+if(FALSE) {
+	require('ROCR')
+
+	logit.fit <- glm(
+		Grant.Status ~ Number.of.Successful.Grant.1 + Number.of.Unsuccessful.Grant.1 + Grant.Application.ID,
+		data = traindat,
+		family = binomial(link = 'logit')
+	)
+
+	GLMPred <- predict(logit.fit,testdat,type = "response")
+
+	print(GLMPred)
+}
